c7adc1785007a3f9a36b10160e678d7d148a43ad
velovan <velovan@users.noreply.github.com>
2021-12-12 14:52:10 +0200

    Update BookList.java

--- Books/src/Book/BookList.java
+++ Books/src/Book/BookList.java
@@ -7,27 +7,27 @@ public class BookList {
 	private ArrayList<Book> bookList;
 
 	public BookList() {
-		bookList = new ArrayList<Book>();
+	     bookList = new ArrayList<Book>();
 	}
 
 	public ArrayList<Book> createBookList() {
-		bookList.add(new Book("The_Hobbit", "valid"));
-		bookList.add(new Book("Anne_of_Green_Gables", "valid"));
-		bookList.add(new Book("Le_Petit_Prince", "valid"));
-		bookList.add(new Book("The_Eagle_Has_Landed", "valid"));
-		bookList.add(new Book("Magic_Tree_House_series", "valid"));
-		bookList.add(new Book("The_Catcher_in_the_Rye", "invalid"));
-		bookList.add(new Book("Tarzan", "invalid"));
+	     bookList.add(new Book("The_Hobbit", "valid"));
+	     bookList.add(new Book("Anne_of_Green_Gables", "valid"));
+	     bookList.add(new Book("Le_Petit_Prince", "valid"));
+	     bookList.add(new Book("The_Eagle_Has_Landed", "valid"));
+	     bookList.add(new Book("Magic_Tree_House_series", "valid"));
+	     bookList.add(new Book("The_Catcher_in_the_Rye", "invalid"));
+	     bookList.add(new Book("Tarzan", "invalid"));
 
-		return bookList;
+	     return bookList;
 	}
 
 	public String getEachBookName(int n) {
-		return bookList.get(n).getBookName();
+	     return bookList.get(n).getBookName();
 	}
 
 	public String getEachBookStatus(int s) {
-		return bookList.get(s).getBookStatus();
+	     return bookList.get(s).getBookStatus();
 	}
 
 }
