f48b971d29b8f77564773d663be15fcc407f1ad9
velovan <velovan@users.noreply.github.com>
2021-12-27 14:35:55 +0200

    Update Init.java

--- Books/src/Book/Init.java
+++ Books/src/Book/Init.java
@@ -3,11 +3,11 @@ package Book;
 public class Init {
 
 	public void initOutput() {
-		Information info = new Information();
-		Menu menu = new Menu();
-		String[] createMenu = menu.createMenuOptions();
-		String listMenu = menu.listMenuOptionsContent(createMenu);
-		String output = info.getWelcomeInformation() + "\n"  + listMenu;
-		System.out.println(output);
+	     Information info = new Information();
+	     Menu menu = new Menu();
+	     String[] createMenu = menu.createMenuOptions();
+	     String listMenu = menu.listMenuOptionsContent(createMenu);
+	     String output = info.getWelcomeInformation() + "\n"  + listMenu;
+	     System.out.println(output);
 	}
 }
