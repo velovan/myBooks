ae6307e3b76acb07a77c08d90745962c512abb32
velovan <velovan@users.noreply.github.com>
2021-12-28 14:07:05 +0200

    Update Main.java

--- Books/src/Book/Main.java
+++ Books/src/Book/Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Init init = new Init();
 		init.initOutput();
-		while (true) {
-			CommandInput comm = new CommandInput();
-			Menu menu = new Menu();
-			String input = comm.readInput();
-			String output = menu.choose1(input);
-			System.out.println(output);
+		while(true){
+		   CommandInput comm = new CommandInput();
+		   Menu menu = new Menu();
+		   String input = comm.readInput();
+		   String output = menu.choose1(input);
+		   System.out.println(output);
 		}
 	}
 
