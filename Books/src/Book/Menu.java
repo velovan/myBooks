3938cd391f0513f15c243d2cbcfadbda9cc00271
velovan <velovan@users.noreply.github.com>
2022-06-19 14:02:10 +0300

    Update Menu.java

--- Books/src/Book/Menu.java
+++ Books/src/Book/Menu.java
@@ -2,20 +2,20 @@ package Book;
 
 import java.util.ArrayList;
 
-public class Menu {
+public class Menu{
 
 	private String[] menuOptions;
 	private String menuOptionsContent;
 	private BookList bookList;
 	private MovieList movieList;
 
-	public Menu() {
+	public Menu(){
 		menuOptions = new String[5];
 		bookList = new BookList();
 		movieList = new MovieList();
 	}
 
-	public String[] createMenuOptions() {
+	public String[] createMenuOptions(){
 		menuOptions[0] = "help: ";
 		menuOptions[1] = "book: ";
 		menuOptions[2] = "reserve Book: ";
@@ -24,14 +24,14 @@ public class Menu {
 		return menuOptions;
 	}
 
-	public String listMenuOptionsContent(String[] menuOptions) {
+	public String listMenuOptionsContent(String[] menuOptions){
 		menuOptionsContent = menuOptions[0] + "  List all menu options\n" + menuOptions[1] + "  View all books\n"
 			+ menuOptions[2].substring(0, 7) + " [--bookName]  Reserve one book called bookName\n" + menuOptions[3]
 			+ "  Check your library number\n" + menuOptions[4] + "View all movie";
 		return menuOptionsContent;
 	}
 
-	public String helpPrintContent(String[] menuOptions) {
+	public String helpPrintContent(String[] menuOptions){
 		String helpPrintContent;
 		helpPrintContent = menuOptions[1] + "  View all books\n" + menuOptions[2].substring(0, 7)
 			+ " [--bookName]  Reserve one book called bookName\n" + menuOptions[3] + "  Check your library number\n"
@@ -39,7 +39,7 @@ public class Menu {
 		return helpPrintContent;
 	}
 
-	public String printBook(ArrayList<Book> bookList) {
+	public String printBook(ArrayList<Book> bookList){
 		String books = "";
 		for (int i = 0; i < bookList.size(); i++) {
 			books += bookList.get(i).getBookName() + " | " + bookList.get(i).getBookStatus() + "\n";
@@ -47,46 +47,46 @@ public class Menu {
 		return books;
 	}
 
-	public String printMovie(ArrayList<Movie> movieList) {
+	public String printMovie(ArrayList<Movie> movieList){
 		String movies = "";
-		for (int i = 0; i < movieList.size(); i++) {
+		for (int i = 0; i < movieList.size(); i++){
 			movies += movieList.get(i).printOneMovie() + "\n";
 		}
 		return movies;
 	}
 
-	public String CheckPrintContent() {
+	public String CheckPrintContent(){
 		Information information = new Information();
 		return information.getCheckLibNumInformation();
 	}
 
-	public String ReserveValidPrintContent() {
+	public String ReserveValidPrintContent(){
 		Information information = new Information();
 		return information.getValidBookInformation();
 	}
 
-	public String ReserveInvalidPrintContent() {
+	public String ReserveInvalidPrintContent(){
 		Information information = new Information();
 		return information.getInvalidBookInformation();
 	}
 
-	public String SelectInvalidMenuOptionPrintContent() {
+	public String SelectInvalidMenuOptionPrintContent(){
 		Information information = new Information();
 		return information.getInvalidOptionInformation();
 	}
 
-	public String choose1(String menuOptions) {
+	public String choose1(String menuOptions){
 		String menu = "";
 		ArrayList<Book> books = bookList.createBookList();
 		ArrayList<Movie> movies = movieList.createMovieList();
 		if (menuOptions.equals("help")) {
 			menu = helpPrintContent(createMenuOptions());
-		} else if (menuOptions.equals("book")) {
+		} else if (menuOptions.equals("book")){
 			menu = printBook(books);
-		} else if (menuOptions.matches("reserve\\s(.*)")) {
+		} else if (menuOptions.matches("reserve\\s(.*)")){
 			for (int i = 0; i < books.size(); i++) {
-				if (menuOptions.split("\\s")[1].equals(books.get(i).getBookName())) {
-					if (books.get(i).getBookStatus().equals("valid")) {
+				if (menuOptions.split("\\s")[1].equals(books.get(i).getBookName())){
+					if (books.get(i).getBookStatus().equals("valid")){
 						menu = ReserveValidPrintContent();
 					} else {
 						menu = ReserveInvalidPrintContent();
@@ -96,9 +96,9 @@ public class Menu {
 					menu = ReserveInvalidPrintContent();
 				}
 			}
-		} else if (menuOptions.equals("movie")) {
+		} else if (menuOptions.equals("movie")){
 			menu = printMovie(movies);
-		} else if (menuOptions.equals("chek")) {
+		} else if (menuOptions.equals("chek")){
 			menu = CheckPrintContent();
 		} else {
 			menu = SelectInvalidMenuOptionPrintContent();
